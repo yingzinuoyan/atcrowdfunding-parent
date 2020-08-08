146f13c6d5fdea2918dcf6fd91d395c6ba73d718
影子 <656202569@qq.com>
2020-08-08 19:25:12 +0800

    众筹项目

---
+++ atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/love.java
@@ -0,0 +1,11 @@
+package com.atguigu.atcrowdfunding;
+
+/**
+ * @author qi
+ * @create 2020-08-08 19:23
+ */
+public class love {
+    public static void main(String[] args) {
+        System.out.println("I love you");
+    }
+}
